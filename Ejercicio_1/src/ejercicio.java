16ecf1a9432741684992128479bcea62b658f5c0
Germán López Pineda <me1lopig@uco.es>
2020-01-02 21:09:10 +0100

    Primer intento de codigo a enviar a git

---
+++ Ejercicio_1/src/ejercicio.java
@@ -0,0 +1,18 @@
+// primera aplicacion
+// aplicacion a enviar a github
+public class ejercicio 
+{
+
+
+	public static void main(String[] args) 
+	{
+		
+		int edad=17;
+		
+		// TODO Auto-generated method stub
+		System.out.println("Hello word\n");
+		System.out.println("La edad es de "+edad+" años\n");
+
+	}
+
+}
